1c6474afdc76ac5bbdcf3997a3d64ea1ff3ae60b
Jon Yu <jj07chm7@gmail.com>
2018-04-17 00:31:49 +0000

    complete variables chapter

---
+++ 02_variables/ex5.rb
@@ -0,0 +1,5 @@
+# x in the first script will return 3
+# x in the second script will return undefined
+# In the second script the x is scoped to the block
+# You do not have access to x outside that block
+# The second x was initialized in the inner scope
---
+++ 02_variables/name.rb
@@ -0,0 +1,7 @@
+puts "Please enter your name"
+name = gets.chomp
+puts "Please enter you last name"
+last_name = gets.chomp
+puts "Hello, #{name} #{last_name}."
+puts "======"
+10.times { puts name }
